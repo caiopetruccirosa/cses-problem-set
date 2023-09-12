541fa44618a82895deffdb72f6588591fb56a35d
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2023-09-12 00:21:38 -0300

    finished introductory problems

--- introduction/chessboard-and-queens.cpp
+++ introduction/chessboard-and-queens.cpp
@@ -7,12 +7,32 @@
 
 using namespace std;
 
+int solve(vector<string> &board, vector<bool> &cols, vector<bool> &diagright, vector<bool> &diagleft, int r) {
+    if (r >= 8)
+        return 1;
+
+    int s = 0;
+    for (int c = 0; c < 8; c++) {
+        if (!cols[c] && !diagleft[r+c] && !diagright[r-c+7] && board[r][c] == '.') {
+            cols[c] = true;
+            diagleft[r+c] = true;
+            diagright[r-c+7] = true;
+            s += solve(board, cols, diagright, diagleft, r+1);
+            cols[c] = false;
+            diagleft[r+c] = false;
+            diagright[r-c+7] = false;
+        }
+    }
+    return s;
+}
+
 int main() {_
+    vector<bool> cols(8, false), diagright(15, false), diagleft(15, false);
     vector<string> board(8);
     for (int i = 0; i < 8; i++)
         cin >> board[i];
 
-    
+    cout << solve(board, cols, diagright, diagleft, 0) << '\n';
 
     return 0;
 }
--- introduction/grid-paths.cpp
+++ introduction/grid-paths.cpp
@@ -6,55 +6,60 @@
 
 using namespace std;
 
-//vector<pii> directions = {pii(0, 1), pii(0, -1), pii(1, 0), pii(-1, 0)};
-vector<char> directions = {'U','D','L','R'};
+int drow[4] = {-1, 1, 0, 0};
+int dcol[4] = {0, 0, -1, 1};
+int path[48];
+bool visited[9][9];
 
-pii can_move(vector<vector<bool>> &used, char d, pii pos) {
-    if (d == 'D' && pos.second < 48 && !used[pos.first][pos.second+1])
-        return pii(pos.first,pos.second+1);
-    else if (d == 'U' && pos.second > 1 && !used[pos.first][pos.second-1])
-        return pii(pos.first,pos.second-1);
-    else if (d == 'L' && pos.first > 1 && !used[pos.first-1][pos.second])
-        return pii(pos.first-1,pos.second);
-    else if (d == 'R' && pos.first < 48 && !used[pos.first+1][pos.second])
-        return pii(pos.first+1,pos.second);
-    return pii(-1,-1);
-}
+int solve(int idx, int r, int c) {
+    // CHECK IF PATH HAS ENDED
+    if (idx >= 48)
+        return 1*(r == 7 && c == 1);
 
-int solve(vector<vector<int>> &paths, vector<vector<bool>> &used, string &s, int i, pii pos) {
-    if (paths[pos.first][pos.second] != -1)
-        return paths[pos.first][pos.second];
+    // CHECK IF HAS REACHED END BEFORE COMPLETE PATH
+    if (r == 7 && c == 1)
+        return 0;
 
-    if (i == s.length()) {
-        if (pos.first == 1 && pos.second == 7)
-            return 1;
-        else
-            return 0;
-    }
+    // CHECK IF POSITION SPLIT THE BOARD
+    if ((
+            visited[r+drow[0]][c+dcol[0]] &&  // cant move U
+            visited[r+drow[1]][c+dcol[1]] &&  // cant move D
+            !visited[r+drow[2]][c+dcol[2]] && // can move L
+            !visited[r+drow[3]][c+dcol[3]]    // can move R
+        )
+        ||
+        (
+            visited[r+drow[2]][c+dcol[2]] &&  // cant move L
+            visited[r+drow[3]][c+dcol[3]] &&  // cant move R
+            !visited[r+drow[0]][c+dcol[0]] && // can move U
+            !visited[r+drow[1]][c+dcol[1]]    // can move D
+        ))
+        return 0;
 
+    // MARK CURRENT POSITION AS VISITED
+    visited[r][c] = true;
+    
     int amount = 0;
-    if (s[i] != '?') {
-        pii next = can_move(used, s[i], pos);
-        if (next.first != -1 && next.second != -1) {
-            cout << "Próxima posição: (" << next.first << "," << next.second << ")" << endl;
-            used[next.first][next.second] = true;
-            amount = solve(paths, used, s, i+1, next);
-            used[next.first][next.second] = false;
+    if (path[idx] < 4) { // FOLLOWS PATH
+        if (!visited[r+drow[path[idx]]][c+dcol[path[idx]]]) {
+            amount += solve(idx+1, r+drow[path[idx]], c+dcol[path[idx]]);
         }
-    } else {
-        for (char d: directions) {
-            pii next = can_move(used, d, pos);
-            if (next.first != -1 && next.second != -1) {
-                cout << "Próxima posição: (" << next.first << "," << next.second << ")" << endl;
-                used[next.first][next.second] = true;
-                amount += solve(paths, used, s, i+1, next);
-                used[next.first][next.second] = false;
-            } 
+    } else { // TRIES DIFFERENTS PATHS
+        for (int i = 0; i < 4; i++) {
+            // CHECK IF PATH IS VISITED
+            if (visited[r+drow[i]][c+dcol[i]]) {
+                continue;
+            }
+
+            // CHECK IF PATH WILL LEAD TO DEADEND
+            // ...
+            
+            amount += solve(idx+1, r+drow[i], c+dcol[i]);
         }
     }
 
-    paths[pos.first][pos.second] = amount;
-
+    visited[r][c] = false;
+    
     return amount;
 }
 
@@ -62,18 +67,29 @@ int main() {_
     string s;
     cin >> s;
 
-    vector<vector<bool>> used(49, vector<bool>(49, false));
-    vector<vector<int>> paths(49, vector<int>(49, -1));
-    
-    int amount = 0;
-    for (int x = 1; x <= 48; x++) {
-        for (int y = 1; y <= 48; y++) {
-            used[x][y] = true;
-            amount += solve(paths, used, s, 0, pii(x,y));
-            used[x][y] = false;
-        }
+    for (int i = 0; i < 48; i++) {
+        if (s[i] == 'U')
+            path[i] = 0;
+        else if (s[i] == 'D')
+            path[i] = 1;
+        else if (s[i] == 'L')
+            path[i] = 2;
+        else if (s[i] == 'R')
+            path[i] = 3;
+        else
+            path[i] = 4; 
     }
-    cout << amount << '\n';
+    for (int i = 0; i < 9; i++) {
+        visited[0][i] = true;
+        visited[8][i] = true;
+        visited[i][0] = true;
+        visited[i][8] = true;
+    }
+    for (int i = 1; i < 8; i++)
+        for (int j = 1; j < 8; j++)
+            visited[i][j] = false;
+
+    cout << solve(0, 1, 1) << '\n';
 
     return 0;
 }
